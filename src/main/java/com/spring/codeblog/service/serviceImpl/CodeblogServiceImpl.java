45ca6fdc782478ab8023cd282c0b0a8ad7dea8d4
Robson <joserobsonsiqueira23@hotmail.com>
2022-03-01 00:17:11 -0300

    Blog Aplicação

---
+++ src/main/java/com/spring/codeblog/service/serviceImpl/CodeblogServiceImpl.java
@@ -0,0 +1,31 @@
+package com.spring.codeblog.service.serviceImpl;
+
+import com.spring.codeblog.model.Post;
+import com.spring.codeblog.repository.CodeblogRepository;
+import com.spring.codeblog.service.CodeblogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class CodeblogServiceImpl implements CodeblogService {
+
+    @Autowired
+    CodeblogRepository codeblogRepository;
+
+    @Override
+    public List<Post> findAll() {
+        return codeblogRepository.findAll();
+    }
+
+    @Override
+    public Post findById(Long id) {
+        return codeblogRepository.findById(id).get();
+    }
+
+    @Override
+    public Post save(Post post) {
+        return codeblogRepository.save(post);
+    }
+}
