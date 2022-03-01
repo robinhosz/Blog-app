a08cacf1a1fed6c0c72a2dfd3074f50e9173b3be
Robson <joserobsonsiqueira23@hotmail.com>
2022-03-01 00:55:59 -0300

    corrigindo bugs

--- src/main/java/com/spring/codeblog/CodeblogApplication.java
+++ src/main/java/com/spring/codeblog/CodeblogApplication.java
@@ -2,9 +2,10 @@ package com.spring.codeblog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CodeblogApplication {
+public class CodeblogApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CodeblogApplication.class, args);
